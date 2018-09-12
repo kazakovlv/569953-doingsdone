52b76d9c7f04af5d71abf724c47ac5a95990f314
Leonid Kazakov <kazakovlv@mail.ru>
2018-09-12 23:32:30 +0300

    Замена строки в таблице на требуемую

--- index.php
+++ index.php
@@ -126,6 +126,19 @@ if($show_complete_tasks == 1) {
                     <tr class="tasks__item task task--completed">
                         <td class="task__select">
                             <label class="checkbox task__checkbox">
+                                <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                                <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                            </label>
+                        </td>
+                        <td class="task__date">10.10.2018</td>
+
+                        <td class="task__controls">
+                        </td>
+                    </tr>
+                    <?php endif; ?>
+                    <!--<tr class="tasks__item task task--completed">
+                        <td class="task__select">
+                            <label class="checkbox task__checkbox">
                                 <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" checked>
                                 <span class="checkbox__text">Сделать главную страницу Дела в порядке</span>
                             </label>
@@ -136,8 +149,7 @@ if($show_complete_tasks == 1) {
                         </td>
 
                         <td class="task__date"></td>
-                    </tr>
-                    <?php endif; ?>
+                    </tr>-->
                 </table>
             </main>
         </div>
