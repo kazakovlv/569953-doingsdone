7e805e6c6fa26af9e11fc7f2976e7e321962ede1
Leonid Kazakov <kazakovlv@mail.ru>
2018-09-26 20:51:53 +0300

    Изменение данных таблицы tasks

--- queries.sql
+++ queries.sql
@@ -32,72 +32,29 @@ INSERT INTO projects (id_user, project_name) VALUES (3, "Ремонт");
 INSERT INTO projects (id_user, project_name) VALUES (3, "Роботизация");
 
 /*Заполнение таблицы tasks*/
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 1, 1, NOW( ), '1970-01-01', 0, 'Техническое задание на забор', '', '2018-10-01' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 1, 2, NOW( ), '1970-01-01', 0, 'Изучение оператора INSERT', '', '2018-10-17' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 1, 2, NOW( ), '1970-01-01', 0, 'Изучение оператора UPDATE', '', '2018-10-18' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 1, 3, NOW( ), '1970-01-01', 0, 'Погрузка мебели', '', '2018-10-17' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 1, 3, NOW( ), '1970-01-01', 0, 'Разгрузка мебели', '', '2018-10-18' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 1, 4, NOW( ), '1970-01-01', 0, 'Помыть окна', '', '2018-10-20' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 1, 4, NOW( ), '1970-01-01', 0, 'Пропылесосить', '', '2018-10-21' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 1, 4, NOW( ), '1970-01-01', 0, 'Выбросить мусор', '', '2018-10-22' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 2, 6, NOW( ), '1970-01-01', 0, 'Разметка сайта', '', '2018-10-10' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 2, 7, NOW( ), '1970-01-01', 0, 'Изучение работы с MySQL', '', '2018-10-12' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 2, 7, NOW( ), '1970-01-01', 0, 'Изучение циклов', '', '2018-10-17' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 2, 9, NOW( ), '1970-01-01', 0, 'Покормить кота', '', '2018-10-18' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 2, 9, NOW( ), '1970-01-01', 0, 'Убрать за котом', '', '2018-10-19' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 2, 10, NOW( ), '1970-01-01', 0, 'Сменить цепь', '', '2018-10-20' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 2, 10, NOW( ), '1970-01-01', 0, 'Починить тормоза', '', '2018-10-21' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 3, 12, NOW( ), '1970-01-01', 0, 'Установка VMWare', '', '2018-10-10' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 3, 12, NOW( ), '1970-01-01', 0, 'Клонирование Windows 2012', '', '2018-10-12' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 3, 14, NOW( ), '1970-01-01', 0, 'Поклейка обоев', '', '2018-10-17' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 3, 14, NOW( ), '1970-01-01', 0, 'Облицовка плиткой', '', '2018-10-18' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 3, 14, NOW( ), '1970-01-01', 0, 'Выравнивание потолка', '', '2018-10-19' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 3, 15, NOW( ), '1970-01-01', 0, 'Настройка робота', '', '2018-10-20' );
-INSERT INTO tasks ( id_user, id_project, date_create, date_completion, `status`, task_name, file_name, date_deadline )
-VALUES
-	( 3, 15, NOW( ), '1970-01-01', 0, 'Роботизация выноса мусора', '', '2018-10-21' );
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (1, 1, 1, '2018-09-23 14:38:04', '1970-01-01 00:00:00', b'0', 'Техническое задание на забор', 'File1.txt', '1970-01-01 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (2, 1, 2, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Изучение оператора INSERT', 'File2.txt', '2018-10-17 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (3, 1, 2, '2018-09-23 22:41:30', '2018-09-24 11:10:33', b'1', 'Изучение оператора UPDATE', 'File3.txt', '2018-09-17 23:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (4, 1, 3, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Погрузка дивана и стиральной машины', 'File4.txt', '2018-09-25 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (5, 1, 3, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Разгрузка мебели', 'File5.txt', '2018-09-18 02:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (6, 1, 4, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Помыть окна', '', '2018-10-20 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (7, 1, 4, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Пропылесосить', '', '2018-10-21 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (8, 1, 4, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Выбросить мусор', 'File6.txt', '2018-10-22 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (9, 2, 6, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Разметка сайта', 'File7.txt', '2018-10-10 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (10, 2, 7, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Изучение работы с MySQL', 'File8.txt', '2018-10-12 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (11, 2, 7, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Изучение циклов', 'File9.txt', '2018-10-17 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (12, 2, 9, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Покормить кота', '', '1970-01-01 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (13, 2, 9, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Убрать за котом', '', '1970-01-01 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (14, 2, 10, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Сменить цепь', '', '2018-10-20 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (15, 2, 10, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Починить тормоза', 'File10.txt', '2018-10-21 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (16, 3, 12, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Установка VMWare', 'File11.txt', '2018-10-10 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (17, 3, 12, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Клонирование Windows 2012', 'File12.txt', '2018-10-12 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (18, 3, 14, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Поклейка обоев', '', '2018-09-28 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (19, 3, 14, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Облицовка плиткой', '', '2018-09-27 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (20, 3, 14, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Выравнивание потолка', '', '2018-09-27 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (21, 3, 15, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Настройка робота', '', '1970-01-01 00:00:00');
+INSERT INTO `tasks`(`id`, `id_user`, `id_project`, `date_create`, `date_completion`, `status`, `task_name`, `file_name`, `date_deadline`) VALUES (22, 3, 15, '2018-09-23 22:41:30', '1970-01-01 00:00:00', b'0', 'Роботизация выноса мусора', '', '1970-01-01 00:00:00');
+
 
 /*Получить список из всех проектов для одного пользователя */
 SELECT
